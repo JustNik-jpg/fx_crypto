ae61331ce3bb0d417807ca2761bfd9bdc7942eea
JustNik <nik.maselkov@gmail.com>
2021-03-24 21:14:10 +0200

    Added multi thread calculations

--- src/multilevel/Dispatcher.java
+++ src/multilevel/Dispatcher.java
@@ -9,7 +9,7 @@ import java.util.Date;
 
 public class Dispatcher {
     public static void main(String... args) {
-        //ArrayList<String> list = Generator.getKeys(13, 4, "1111");
+        //ArrayList<String> list = Generator.getKeys(31, 3, "111");
         //System.out.println(list + "\n" + list.size());
         keysOut();
 
@@ -17,34 +17,36 @@ public class Dispatcher {
 
     public static void keysOut() {
         int[] pArray = {3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
-        File file = new File("keys.txt");
+
         String phase = "11111111111111111111";
 
 
-        try (PrintWriter writer = new PrintWriter(file)) {
+        int nCeiling = 9;
 
-            int nCeiling = 9;
+        for (int p : pArray) {
+            Thread t = new Thread(() -> {
+                File file = new File("keys"+p+".txt");
+                try (PrintWriter writer = new PrintWriter(file)) {
 
-            for (int p : pArray) {
-                for (int n = 3; n < nCeiling; n++) {
-                    String head = "p=" + p + " n=" + n;
-                    ArrayList<String> keys = Generator.getKeys(p, n, phase.substring(0, n));
+                    for (int n = 3; n < nCeiling; n++) {
+                        String head = "p=" + p + " n=" + n;
+                        ArrayList<String> keys = Generator.getKeys(p, n, phase.substring(0, n));
 
-                    writer.write(head + "\n");
-                    for (String key : keys) {
-                        writer.write(key + "\n");
+                        writer.write(head + "\n");
+                        for (String key : keys) {
+                            writer.write(key + "\n");
+                        }
+
                     }
-
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                    return;
                 }
 
+            });
+            t.start();
 
-
-            }
-        } catch (FileNotFoundException fileNotFoundException) {
-            fileNotFoundException.printStackTrace();
-            return;
         }
-
     }
 
 }
